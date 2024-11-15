0f2f5c35abf8ce779f1061a703a98ffff98053d7
Marcin Pilarczyk <marcinpilarczyk@me.com>
2024-11-15 14:30:51 +0100

    Add filtering option
    Filtering option by using queryParams and controller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -15,8 +15,19 @@ class ProjectController extends Controller
     public function index()
     {
         $query = Project::query();
+
+        if (request('name')) {
+            $query->where('name', 'like', '%' . request('name') . '%');
+        }
+        if (request('status')) {
+            $query->where('status', request('status'));
+        }
+
         $projects = $query->paginate(10);
-        return inertia("Project/index", ['projects' => ProjectResource::collection($projects)]);
+        return inertia("Project/index", [
+            'projects' => ProjectResource::collection($projects),
+            'queryParams' => request()->query() ?: null,
+        ]);
     }
 
     /**
