bb5e523aac39001a238643541f29e4db6724ad1f
Marcin Pilarczyk <marcinpilarczyk@me.com>
2024-11-15 17:20:47 +0100

    Update index.jsx

--- resources/js/Pages/Project/index.jsx
+++ resources/js/Pages/Project/index.jsx
@@ -27,7 +27,7 @@ export default function Index({ projects, queryParams = null }) {
     <AuthenticatedLayout
       header={
         <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-          Projects 123
+          Projects
         </h2>
       }
     >
